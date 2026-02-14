c797767db72261c475f62214427c87cbf3205b07
Dean Wan <dean@prodincident.com>
2026-02-14 14:47:31 +1100

    test: add unit tests

---
+++ spec/array_search_algorithms_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/algorithms/array_search_algorithms"
+
+describe ArraySearchAlgorithms do
+  describe ".binary_search" do
+    context "given a sorted array" do
+      arr = ArraySearchAlgorithms.new([1, 2, 3, 4, 5, 6])
+      it "returns the index of the element when the element exists" do
+        expect(arr.binary_search(4)).to eq(3)
+      end
+      it "returns nil when the element does not" do
+        expect(arr.binary_search(14)).to eq(nil)
+      end
+    end
+  end
+end
