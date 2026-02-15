61af866a17e5f19faedf3dcdbbfb88cebd23fed9
Dean Wan <dean@prodincident.com>
2026-02-16 09:19:02 +1100

    feat: add insertion sort

--- spec/sort_algorithms_spec.rb
+++ spec/sort_algorithms_spec.rb
@@ -22,4 +22,14 @@ describe SearchAlgorithms do
       expect(arr.array).to eq([3, 4, 1, 7, 8, 5])
     end
   end
+
+  describe '#insertion_sort' do
+    arr = SortAlgorithms.new([3, 4, 1, 7, 8, 5])
+    it 'returns a sorted array' do
+      expect(arr.insertion_sort).to eq([1, 3, 4, 5, 7, 8])
+    end
+    it 'does not modify the original array' do
+      expect(arr.array).to eq([3, 4, 1, 7, 8, 5])
+    end
+  end
 end
