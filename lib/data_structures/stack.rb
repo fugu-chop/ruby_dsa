9dc2877aac91969c7a25a31a0965b9ce781d60ce
Dean Wan <dean@prodincident.com>
2026-02-17 14:48:33 +1100

    refactor: use inbuilt API

--- lib/data_structures/stack.rb
+++ lib/data_structures/stack.rb
@@ -12,24 +12,19 @@ class Stack
   # @return [Any] - returns the element added
   def push(element)
     @stack << element
-
-    element
   end
 
   # pop removes an element from the stack and returns it
   #
   # @return [Any] - returns the last element on the stack
   def pop
-    last = @stack[@stack.length - 1]
-    @stack = @stack[0..@stack.length - 2]
-
-    last
+    @stack.pop
   end
 
   # read returns the last element on the stack
   #
   # @return [Any] - returns the last element on the stack
   def read
-    @stack[@stack.length - 1]
+    @stack.last
   end
 end
