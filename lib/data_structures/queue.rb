9c255c720eb05d1b9d4dc176593f0ea82470bab6
Dean Wan <dean@prodincident.com>
2026-02-17 15:10:30 +1100

    feat: add queue

---
+++ lib/data_structures/queue.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Queue represents the queue data structure
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  # enqueue adds an element to the end of the queue
+  #
+  # @param element [Any] - the element to add to the queue
+  # @return [Any] - returns the element added
+  def enqueue(element)
+    @queue << element
+  end
+
+  # dequeue removes the first item of the queue
+  # and returns it
+  #
+  # @return [Any] - returns the element added
+  def dequeue
+    @queue.shift
+  end
+
+  # read returns the first item of the queue
+  #
+  # @return [Any] - returns the element added
+  def read
+    @queue.first
+  end
+end
