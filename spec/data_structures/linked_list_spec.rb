ef1c260a2e335675e9251a5d8afa51b35c8d7a8b
Dean Wan <dean@prodincident.com>
2026-02-22 18:29:37 +1100

    test: add spec for insert method

--- spec/data_structures/linked_list_spec.rb
+++ spec/data_structures/linked_list_spec.rb
@@ -4,15 +4,15 @@ require './lib/data_structures/linked_list'
 require './lib/data_structures/node'
 
 describe LinkedList do
-  a = Node.new('goodbye')
-  b = Node.new('cruel')
-  c = Node.new('world')
-  a.next = b
-  b.next = c
-  l = LinkedList.new(a)
-
   describe '#read' do
     context 'given a linked list' do
+      a = Node.new('goodbye')
+      b = Node.new('cruel')
+      c = Node.new('world')
+      a.next = b
+      b.next = c
+      l = LinkedList.new(a)
+
       it 'returns the node at idx position when it exists' do
         expect(l.read(2)).to eq('world')
       end
@@ -27,11 +27,58 @@ describe LinkedList do
 
   describe '#index_of' do
     context 'given a linked list' do
+      a = Node.new('goodbye')
+      b = Node.new('cruel')
+      c = Node.new('world')
+      a.next = b
+      b.next = c
+      l = LinkedList.new(a)
+
       it 'returns the index of the node with a matching value' do
         expect(l.index_of('world')).to eq(2)
       end
       it 'returns nil when a value does not match' do
         expect(l.read(99)).to eq(nil)
+      end
+    end
+  end
+
+  describe '#insert' do
+    context 'given a linked list' do
+      it 'handles insertion at index 0' do
+        a = Node.new('goodbye')
+        b = Node.new('cruel')
+        c = Node.new('world')
+        a.next = b
+        b.next = c
+        l = LinkedList.new(a)
+
+        expect(l.insert(0, 'munch')).not_to eq(nil)
+        expect(l.head).not_to eq(nil)
+        expect(l.head.value).to eq('munch')
+        expect(l.head.next).not_to eq(nil)
+        expect(l.head.next.value).to eq('goodbye')
+      end
+      it 'handles insertion at an index that exists' do
+        a = Node.new('goodbye')
+        b = Node.new('cruel')
+        c = Node.new('world')
+        a.next = b
+        b.next = c
+        l = LinkedList.new(a)
+
+        expect(l.insert(2, 'munch')).not_to eq(nil)
+        expect(l.index_of('goodbye')).to eq(0)
+        expect(l.index_of('cruel')).to eq(1)
+        expect(l.index_of('munch')).to eq(2)
+        expect(l.index_of('world')).to eq(3)
+      end
+      it 'handles insertion at an index that does not exist' do
+        a = Node.new('goodbye')
+        l = LinkedList.new(a)
+
+        expect(l.insert(99, 'what')).to eq(nil)
+        expect(l.insert(-9, 'what')).to eq(nil)
       end
     end
   end
