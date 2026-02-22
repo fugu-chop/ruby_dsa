ffe584227382bbea5d1c5f4dda26fbe52ee09e95
Dean Wan <dean@prodincident.com>
2026-02-23 08:03:43 +1100

    refactor: extract repeated setup to helper

--- spec/data_structures/linked_list_spec.rb
+++ spec/data_structures/linked_list_spec.rb
@@ -4,22 +4,27 @@ require './lib/data_structures/linked_list'
 require './lib/data_structures/node'
 
 describe LinkedList do
+  def create_linked_list
+    a = Node.new('goodbye')
+    b = Node.new('cruel')
+    c = Node.new('world')
+    a.next = b
+    b.next = c
+    LinkedList.new(a)
+  end
+
   describe '#read' do
     context 'given a linked list' do
-      a = Node.new('goodbye')
-      b = Node.new('cruel')
-      c = Node.new('world')
-      a.next = b
-      b.next = c
-      l = LinkedList.new(a)
-
       it 'returns the node at idx position when it exists' do
+        l = create_linked_list
         expect(l.read(2)).to eq('world')
       end
       it 'handles negative indices' do
+        l = create_linked_list
         expect(l.read(-2)).to eq(nil)
       end
       it 'returns nil when the node does not exist' do
+        l = create_linked_list
         expect(l.read(99)).to eq(nil)
       end
     end
@@ -27,17 +32,12 @@ describe LinkedList do
 
   describe '#index_of' do
     context 'given a linked list' do
-      a = Node.new('goodbye')
-      b = Node.new('cruel')
-      c = Node.new('world')
-      a.next = b
-      b.next = c
-      l = LinkedList.new(a)
-
       it 'returns the index of the node with a matching value' do
+        l = create_linked_list
         expect(l.index_of('world')).to eq(2)
       end
       it 'returns nil when a value does not match' do
+        l = create_linked_list
         expect(l.read(99)).to eq(nil)
       end
     end
@@ -46,12 +46,7 @@ describe LinkedList do
   describe '#insert_at' do
     context 'given a linked list' do
       it 'handles insertion at index 0' do
-        a = Node.new('goodbye')
-        b = Node.new('cruel')
-        c = Node.new('world')
-        a.next = b
-        b.next = c
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.insert_at(0, 'munch')).not_to eq(nil)
         expect(l.head).not_to eq(nil)
@@ -60,12 +55,7 @@ describe LinkedList do
         expect(l.head.next.value).to eq('goodbye')
       end
       it 'handles insertion at an index that exists' do
-        a = Node.new('goodbye')
-        b = Node.new('cruel')
-        c = Node.new('world')
-        a.next = b
-        b.next = c
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.insert_at(2, 'munch')).not_to eq(nil)
         expect(l.index_of('goodbye')).to eq(0)
@@ -74,8 +64,7 @@ describe LinkedList do
         expect(l.index_of('world')).to eq(3)
       end
       it 'handles insertion at an index that does not exist' do
-        a = Node.new('goodbye')
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.insert_at(99, 'what')).to eq(nil)
         expect(l.insert_at(-9, 'what')).to eq(nil)
@@ -86,12 +75,7 @@ describe LinkedList do
   describe '#delete_at' do
     context 'given a linked list' do
       it 'handles deletion at index 0' do
-        a = Node.new('goodbye')
-        b = Node.new('cruel')
-        c = Node.new('world')
-        a.next = b
-        b.next = c
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.delete_at(0)).not_to eq(nil)
         expect(l.head).not_to eq(nil)
@@ -101,20 +85,14 @@ describe LinkedList do
         expect(l.head.next.next).to eq(nil)
       end
       it 'handles deletion at an index that exists' do
-        a = Node.new('goodbye')
-        b = Node.new('cruel')
-        c = Node.new('world')
-        a.next = b
-        b.next = c
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.delete_at(1)).not_to eq(nil)
         expect(l.index_of('goodbye')).to eq(0)
         expect(l.index_of('world')).to eq(1)
       end
       it 'handles deletion at an index that does not exist' do
-        a = Node.new('goodbye')
-        l = LinkedList.new(a)
+        l = create_linked_list
 
         expect(l.delete_at(99)).to eq(nil)
         expect(l.delete_at(-9)).to eq(nil)
