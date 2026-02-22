56d82438ee8e796dc7683201a16a5f00ac02ef79
Dean Wan <dean@prodincident.com>
2026-02-23 08:19:05 +1100

    feat: add doubly linked node

--- lib/data_structures/node.rb
+++ lib/data_structures/node.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 # Node represents the node data structure
-# used in Linked Lists and Trees
+# used in Linked Lists, Queues, Stacks
+# and Trees.
 class Node
   attr_reader :value
   attr_accessor :next
+
+  def initialize(value)
+    @value = value
+  end
+end
+
+# Node represents the double linked node
+# data structure used in Doubly Linked Lists.
+class DoubleLinkedNode
+  attr_reader :value
+  attr_accessor :next, :prev
 
   def initialize(value)
     @value = value
