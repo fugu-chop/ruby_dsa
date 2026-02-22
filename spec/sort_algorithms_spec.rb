cb6f49577a516dab38ccf19bc400de7836c4f62e
Dean Wan <dean@prodincident.com>
2026-02-22 14:18:38 +1100

    test: add spec for merge_sort

--- spec/sort_algorithms_spec.rb
+++ spec/sort_algorithms_spec.rb
@@ -34,4 +34,14 @@ describe SearchAlgorithms do
       expect(arr.array).to eq([1, 3, 4, 5, 7, 8])
     end
   end
+
+  describe '#merge_sort' do
+    arr = SortAlgorithms.new([3, 4, 1, 7, 8, 5])
+    it 'returns a sorted array' do
+      expect(arr.merge_sort).to eq([1, 3, 4, 5, 7, 8])
+    end
+    it 'does not mutate the array' do
+      expect(arr.array).to eq([3, 4, 1, 7, 8, 5])
+    end
+  end
 end
