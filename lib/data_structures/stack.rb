e94df25a9c98d2f89f4e8ff50750b1a531556401
Dean Wan <dean@prodincident.com>
2026-02-24 14:12:04 +1100

    refactor: use Node type for stack

--- lib/data_structures/stack.rb
+++ lib/data_structures/stack.rb
@@ -2,31 +2,36 @@
 
 # Stack represents the stack data structure
 class Stack
-  def initialize
-    # TODO: Replace with Node type
-    @stack = []
+  def initialize(value)
+    @head = Node.new(value)
   end
 
   # push adds an element to the stack
   #
   # @param element [Any] - the element to add to the stack
-  # @return [Any] - returns the element added
+  # @return [Node] - returns the Node added
   def push(element)
-    @stack << element
-    element
+    new_node = Node.new(element)
+    new_node.next = @head
+    @head = new_node
   end
 
-  # pop removes an element from the stack and returns it
+  # pop removes a Node from the stack and returns it
   #
-  # @return [Any] - returns the last element on the stack
+  # @return [Node, nil] - returns the Node on the stack
+  # or nil if there is no Node
   def pop
-    @stack.pop
+    popped = @head
+    @head = @head&.next
+
+    popped
   end
 
-  # read returns the last element on the stack
+  # read returns the top element on the stack
   #
-  # @return [Any] - returns the last element on the stack
+  # @return [Any, nil] - returns the last element on the stack
+  # or nil if there is no Node on the stack
   def read
-    @stack.last
+    @head&.value
   end
 end
