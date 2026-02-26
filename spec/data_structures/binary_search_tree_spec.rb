5aecc4d413b2bbbd9be6e1247ced35bd3001a868
Dean Wan <dean@prodincident.com>
2026-02-27 09:09:44 +1100

    spec: add tests for search

--- spec/data_structures/binary_search_tree_spec.rb
+++ spec/data_structures/binary_search_tree_spec.rb
@@ -34,4 +34,26 @@ describe BinarySearchTree do
       end
     end
   end
+
+  describe '#search' do
+    context 'searching for a value' do
+      it 'returns the node when the value exists' do
+        bst = create_bst
+
+        fifty_six = bst.search(56)
+        expect(fifty_six).not_to eq(nil)
+        expect(fifty_six.value).to eq(56)
+
+        eighty_nine = bst.search(89)
+        expect(eighty_nine).not_to eq(nil)
+        expect(eighty_nine.value).to eq(89)
+      end
+
+      it 'returns nil if it does not exist' do
+        bst = create_bst
+
+        expect(bst.search(99)).to eq(nil)
+      end
+    end
+  end
 end
