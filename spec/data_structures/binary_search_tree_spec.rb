207ea6a2ebb677a187d7914091c863dc97fa02c2
Dean Wan <dean@prodincident.com>
2026-02-27 09:00:33 +1100

    test: refactor insertion tests

--- spec/data_structures/binary_search_tree_spec.rb
+++ spec/data_structures/binary_search_tree_spec.rb
@@ -4,18 +4,30 @@ require './lib/data_structures/binary_search_tree'
 require './lib/data_structures/node'
 
 describe BinarySearchTree do
+  def create_bst
+    b = BinarySearchTree.new(50)
+    b.insert(50)
+    b.insert(25)
+    b.insert(75)
+    b.insert(10)
+    b.insert(33)
+    b.insert(56)
+    b.insert(89)
+    b
+  end
+
   describe '#insert' do
     context 'insertion of a node' do
       it 'returns a node when the node value does not exist' do
-        bst = BinarySearchTree.new(50)
+        bst = create_bst
 
-        result = bst.insert(5)
+        result = bst.insert(4)
         expect(result).not_to eq(nil)
-        expect(result.value).to eq(5)
+        expect(result.value).to eq(4)
       end
 
       it 'returns nil when the value of the node already exists' do
-        bst = BinarySearchTree.new(50)
+        bst = create_bst
 
         result = bst.insert(50)
         expect(result).to eq(nil)
