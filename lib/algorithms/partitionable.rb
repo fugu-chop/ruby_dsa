0f0a1801b2fbaffe37af0fec0b8217d100ab5f06
Dean Wan <dean@prodincident.com>
2026-03-03 23:34:39 +1100

    feat: fix signatures and random partition

--- lib/algorithms/partitionable.rb
+++ lib/algorithms/partitionable.rb
@@ -3,7 +3,7 @@
 # The Partitionable module provides a partition function designed for
 # algorithms that rely on partitioning (quickselect and quicksort).
 module Partitionable
-  # partition ensures that the element of an array
+  # partition! ensures that the element of an array
   # at a 'pivot' index (always set to the last
   # element of the array) is correctly placed within
   # the context of an array (ascending order).
@@ -35,5 +35,27 @@ module Partitionable
 
     # return what is the new pivot index
     left_ptr
+  end
+
+  # random_partition! ensures that the element of an
+  # array at a 'pivot' index is correctly placed within
+  # the context of an array (ascending order).
+  # It differs from `partition!` in that it randomly
+  # selects a partition index and swaps it to the end
+  # of the array to avoid quadratic time complexity
+  # when the array is largely pre-sorted.
+  #
+  # @param left_ptr [Integer] - the left index where
+  # the partition process begins
+  # @param right_ptr [Integer] - the right index where
+  # the partition process begins
+  # @return [Integer] - the new pivot index
+  def random_partition!(arr, left_ptr, right_ptr)
+    random_pivot_idx = rand(left_ptr..right_ptr)
+
+    arr[random_pivot_idx], arr[right_ptr] =
+      arr[right_ptr], arr[random_pivot_idx]
+
+    partition!(arr, left_ptr, right_ptr)
   end
 end
