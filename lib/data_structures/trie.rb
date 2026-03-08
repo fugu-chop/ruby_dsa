35706cdb706001d53120beae5331ed1d18beaea0
Dean Wan <dean@prodincident.com>
2026-03-08 20:08:40 +1100

    feat: add search function

--- lib/data_structures/trie.rb
+++ lib/data_structures/trie.rb
@@ -9,6 +9,25 @@ class Trie
     @root = TrieNode.new
   end
 
+  # search attempts to find a word within the Trie
+  # structure.
+  #
+  # @return [TrieNode, nil] - Return the `*` TrieNode
+  # associated with the word, or nil if the word is not
+  # found
+  def search(word)
+    current_node = root
+
+    word.each_char do |letter|
+      result = current_node.get(letter)
+      return nil unless result
+
+      current_node = result
+    end
+
+    current_node
+  end
+
   # insert attempts to add a word to the trie
   # by converting each letter of a word to a
   # TrieNode class.
--- spec/data_structures/trie_spec.rb
+++ spec/data_structures/trie_spec.rb
@@ -3,6 +3,30 @@
 require './lib/data_structures/trie'
 
 describe Trie do
+  describe '#search' do
+    context 'given a trie' do
+      it 'returns nil when the word does not exist' do
+        trie = Trie.new
+        expect(trie.insert('bonk')).to eq(nil)
+
+        expect(trie.search('value')).to eq(nil)
+      end
+      it 'returns nil when the trie is empty' do
+        trie = Trie.new
+
+        expect(trie.search('value')).to eq(nil)
+      end
+      it 'returns a TrieNode when the word does exist' do
+        trie = Trie.new
+
+        expect(trie.insert('value')).to eq(nil)
+
+        result = trie.search('value')
+        expect(result).not_to eq(nil)
+        expect(result.get('*')).not_to eq(nil)
+      end
+    end
+  end
   describe '#insert' do
     context 'given a trie' do
       it 'inserts a letter when it does not already exist' do
